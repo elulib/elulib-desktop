59877c0c9c0973611b29403599d10c0886e849d1
gsbm <31590269+gsbm@users.noreply.github.com>
2025-12-22 21:22:17 +0100

    Fix macOS code signing failure

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,13 +3,13 @@
 //! This file contains only the main entry point that delegates
 //! initialization and execution to the main library.
 
+// Prevent displaying an additional console window on Windows in release mode
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 /// Main function of the application
 ///
-/// Disables the console window on Windows in release mode
-/// and delegates execution to the main library.
+/// Delegates execution to the main library.
 fn main() {
-    // Prevent displaying an additional console window on Windows
-    #[cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
     // Delegate to the main application logic
     // If the application cannot start, run() will terminate with an error code
     elulib_desktop::run();
